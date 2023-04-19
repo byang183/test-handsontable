9e83420229960fb9a579bbc58aa88854490f711e
Bo Yang <byang183@bloomberg.net>
2023-04-19 16:00:00 -0400

    add issue example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { HotTable } from "@handsontable/react";
+import { useState } from "react";
+import { useRef } from "react";
+import { registerAllModules } from "handsontable/registry";
+import "handsontable/dist/handsontable.full.css";
+import styled from "styled-components";
+
+registerAllModules();
 
 function App() {
+  const hotRef = useRef(null);
+  // if set collapsed to false, handsontable has no issue at all when collapse or expand
+  const [collapsed, updateCollapsed] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Wrapper className="App">
+      <div>
+        <button
+          onClick={() => {
+            updateCollapsed(!collapsed);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {collapsed ? "collapsed" : "expanded"}
+        </button>
+      </div>
+      <div className={collapsed ? "collapsed" : undefined}>
+        <HotTable
+          contextMenu={true}
+          colHeaders={["url", "content type"]}
+          rowHeaders={true}
+          ref={hotRef}
+          columns={[
+            {
+              renderer: (instance, TD, row, col, prop, value) => {
+                TD.innerHTML = `<span>${value}</span>`;
+              },
+            },
+            {
+              type: "dropdown",
+              source: ["text/html", "application/pdf"],
+              renderer: (instance, TD, row, col, prop, value) => {
+                TD.innerHTML = `<span }'>${value || ""}</span>`;
+              },
+            },
+          ]}
+          height={300}
+          stretchH="all"
+          colWidths={[450, 150]}
+          minSpareRows={1}
+        />
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  max-width: 60em;
+  margin-left: 3em;
+  th {
+    background-color: darkgray;
+  }
+
+  td,
+  th {
+    border-bottom-color: gray;
+  }
+
+  .collapsed {
+    display: none;
+  }
+`;
 
 export default App;
